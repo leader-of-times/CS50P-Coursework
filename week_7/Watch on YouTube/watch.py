52add311b32f60e2a865acc32553044f558cc440
Mohammad Noufal <mohammadnoufalctr@gmail.com>
2024-09-26 12:28:32 +0530

    week 7

---
+++ week_7/Watch on YouTube/watch.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+def main():
+    print(parse(input("HTML: ")))
+
+def parse(s):
+    pattern = r'<iframe .*?src="(?:https?:)?//(?:www\.)?youtube\.com/embed/([^/"]+)"'
+
+    match = re.search(pattern, s)
+
+    if match:
+        return f"https://youtu.be/{match.group(1)}"
+    else:
+        return None
+
+if __name__ == "__main__":
+    main()
